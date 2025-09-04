e4c7785d1b1ccd1b48797de28f259ae1911b5200
miguel2433 <miguelitocap12@gmail.com>
2025-09-04 16:16:57 -0300

    tailwindcss add

---
+++ src/SpotifyMVC/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./Pages/**/*.cshtml",
+    "./Views/**/*.cshtml",
+    "./Areas/**/Views/**/*.cshtml",
+    "./wwwroot/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
