36f1957b2672ac7e86a2730f2761cb52b22494b4
miguel2433 <miguelitocap12@gmail.com>
2025-10-08 23:46:53 -0300

    me canse

--- scripts sql/Insert.sql
+++ scripts sql/Insert.sql
@@ -1,17 +1,17 @@
 USE 5to_Spotify;
 
 -- Tabla Artista
- CALL altaArtista('some.jpg','TripleT', 'Miguel', 'Verduguez', @idArtistaTripleT);
- CALL altaArtista('some.jpg','El Chapo', 'Josu', 'Duran', @idArtistaElChapo);
- CALL altaArtista('some.jpg','La Maquinaria', 'Rene', 'Terrazas', @idArtistalaMaquinaria);
- CALL altaArtista('some.jpg','El Renacido', 'Leonardo', 'Cheng', @idArtistaElRenacido);
+ CALL altaArtista('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','TripleT', 'Miguel', 'Verduguez', @idArtistaTripleT);
+ CALL altaArtista('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','El Chapo', 'Josu', 'Duran', @idArtistaElChapo);
+ CALL altaArtista('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','La Maquinaria', 'Rene', 'Terrazas', @idArtistalaMaquinaria);
+ CALL altaArtista('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','El Renacido', 'Leonardo', 'Cheng', @idArtistaElRenacido);
 
 -- Tabla Album
 -- Álbumes con fechas de ejemplo
-CALL altaAlbum(@idAlbumEcos,'Ecos del Pasado',@idArtistaElRenacido,'2018-05-20','some.jpg');
-CALL altaAlbum(@idAlbumSuenios,'Sueños de Verano',@idArtistalaMaquinaria,'2019-08-10','some.jpg');
-CALL altaAlbum(@idAlbumCaminos,'Caminos Cruzados',@idArtistaElChapo,'2020-03-15','some.jpg');
-CALL altaAlbum(@idAlbumLuz,'Luz y Sombra',@idArtistaTripleT,'2021-11-05','some.jpg');
+CALL altaAlbum(@idAlbumEcos,'Ecos del Pasado',@idArtistaElRenacido,'2018-05-20','1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png');
+CALL altaAlbum(@idAlbumSuenios,'Sueños de Verano',@idArtistalaMaquinaria,'2019-08-10','1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png');
+CALL altaAlbum(@idAlbumCaminos,'Caminos Cruzados',@idArtistaElChapo,'2020-03-15','1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png');
+CALL altaAlbum(@idAlbumLuz,'Luz y Sombra',@idArtistaTripleT,'2021-11-05','1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png');
 
 -- Tabla Nacionalidad
 CALL altaNacionalidad ('Argentina', @idNacionalidadArgentina);
@@ -32,10 +32,10 @@ CALL altaGenero('Reggae', @idGeneroReggae);
 CALL altaGenero('Ranchera', @idGeneroRanchera);
 
 -- Tabla Cancion
-CALL altaCancion(@idCancionOver, 'some.jpg','Its Over, Isnt It', '00:02:17', @idAlbumSuenios, @idArtistaElRenacido, @idGeneroHipHop);
-CALL altaCancion(@idCancionRene, 'some.jpg','René', '00:07:41', @idAlbumCaminos, @idArtistaElChapo, @idGeneroJazz);
-CALL altaCancion(@idCancionEstrella, 'some.jpg','Como Estrella', '00:03:40', @idAlbumLuz, @idArtistalaMaquinaria, @idGeneroReggae);
-CALL altaCancion(@idCancionCelos,'some.jpg', 'Estos Celos', '00:03:10', @idAlbumEcos, @idArtistaTripleT, @idGeneroRanchera);
+CALL altaCancion(@idCancionOver, '1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Its Over, Isnt It', '00:02:17', @idAlbumSuenios, @idArtistaElRenacido, @idGeneroHipHop);
+CALL altaCancion(@idCancionRene, '1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','René', '00:07:41', @idAlbumCaminos, @idArtistaElChapo, @idGeneroJazz);
+CALL altaCancion(@idCancionEstrella, '1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Como Estrella', '00:03:40', @idAlbumLuz, @idArtistalaMaquinaria, @idGeneroReggae);
+CALL altaCancion(@idCancionCelos,'1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png', 'Estos Celos', '00:03:10', @idAlbumEcos, @idArtistaTripleT, @idGeneroRanchera);
 
 -- Tabla Historial Reproduccion
 CALL altaHistorial_reproduccion(@idHistorialMiguel, @idUsuarioMiguel, @idCancionOver, '2024-07-01 10:00:00');
@@ -44,10 +44,10 @@ CALL altaHistorial_reproduccion(@idHistorialRene, @idUsuarioRene, @idCancionEstr
 CALL altaHistorial_reproduccion(@idHistorialCheng, @idUsuarioCheng, @idCancionCelos, '2024-07-01 13:00:00');
 
 -- Tabla Playlist
-CALL altaPlaylist('some.jpg','Éxitos de Rock', @idUsuarioMiguel,@idPlaylistRock);
-CALL altaPlaylist('some.jpg','Clásicos del Pop', @idUsuarioJosu,@idPlaylistPop);
-CALL altaPlaylist('some.jpg','Vibras de Jazz', @idUsuarioRene,@idPlaylistJazz);
-CALL altaPlaylist('some.jpg','Ritmos Chill', @idUsuarioCheng, @idPlaylistChill);
+CALL altaPlaylist('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Éxitos de Rock', @idUsuarioMiguel,@idPlaylistRock);
+CALL altaPlaylist('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Clásicos del Pop', @idUsuarioJosu,@idPlaylistPop);
+CALL altaPlaylist('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Vibras de Jazz', @idUsuarioRene,@idPlaylistJazz);
+CALL altaPlaylist('1f2b40ca-7d4f-4d01-91df-68865ab488c7_Screenshot_1.png','Ritmos Chill', @idUsuarioCheng, @idPlaylistChill);
 
 -- Tabla TipoSuscripcion
 CALL altaTipoSuscripcion(@idSuscripcionMensual,1,8,"Mensual");
